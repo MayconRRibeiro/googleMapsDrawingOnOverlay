60d62919e995d1274da97053edaa272b5514132b
Maycon Santos <maycon.santos@db1.com.br>
2022-12-26 02:04:22 -0300

    Drawing Tools + Custom Overlay

--- index.ts
+++ index.ts
@@ -1,34 +1,87 @@
-/**
- * @license
- * Copyright 2019 Google LLC. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-
-// This example requires the Drawing library. Include the libraries=drawing
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=drawing">
-
 function initMap(): void {
   const map = new google.maps.Map(
     document.getElementById("map") as HTMLElement,
     {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8,
+      zoom: 11,
+      center: { lat: 62.323907, lng: -150.109291 },
     }
   );
 
+  const bounds = new google.maps.LatLngBounds(
+    new google.maps.LatLng(62.281819, -150.287132),
+    new google.maps.LatLng(62.400471, -150.005608)
+  );
+  // #1 - Trocar imagem
+  const image = "https://developers.google.com/maps/documentation/javascript/examples/full/images/talkeetna.png";
+
+  class CustomOverlay extends google.maps.OverlayView {
+    bounds;
+    image;
+    div;
+    constructor(bounds, image) {
+      super();
+      this.bounds = bounds;
+      this.image = image;
+    }
+    /**
+     * onAdd is called when the map's panes are ready and the overlay has been
+     * added to the map.
+     */
+    onAdd() {
+      this.div = document.createElement("div");
+      this.div.style.borderStyle = "none";
+      this.div.style.borderWidth = "0px";
+      this.div.style.position = "absolute";
+
+      // Create the img element and attach it to the div.
+      const img = document.createElement("img");
+
+      img.src = this.image;
+      img.style.width = "100%";
+      img.style.height = "100%";
+      img.style.position = "absolute";
+      this.div.appendChild(img);
+
+      // Add the element to the "overlayLayer" pane.
+      const panes = this.getPanes();
+
+      panes?.overlayLayer.appendChild(this.div);
+    }
+    draw() {
+      // We use the south-west and north-east
+      // coordinates of the overlay to peg it to the correct position and size.
+      // To do this, we need to retrieve the projection from the overlay.
+      const overlayProjection = this.getProjection();
+      // Retrieve the south-west and north-east coordinates of this overlay
+      // in LatLngs and convert them to pixel coordinates.
+      // We'll use these coordinates to resize the div.
+      const sw = overlayProjection.fromLatLngToDivPixel(
+        this.bounds.getSouthWest()
+      );
+      const ne = overlayProjection.fromLatLngToDivPixel(
+        this.bounds.getNorthEast()
+      );
+
+      // Resize the image's div to fit the indicated dimensions.
+      if (this.div) {
+        this.div.style.left = sw!.x + "px";
+        this.div.style.top = ne!.y + "px";
+        this.div.style.width = ne!.x - sw!.x + "px";
+        this.div.style.height = sw!.y - ne!.y + "px";
+      }
+    }
+  }
+
+  const overlay = new CustomOverlay(bounds, image);
+  overlay.setMap(map);
+
+  // https://developers.google.com/maps/documentation/javascript/examples/drawing-tools
   const drawingManager = new google.maps.drawing.DrawingManager({
     drawingMode: google.maps.drawing.OverlayType.MARKER,
     drawingControl: true,
     drawingControlOptions: {
       position: google.maps.ControlPosition.TOP_CENTER,
-      drawingModes: [
-        google.maps.drawing.OverlayType.MARKER,
-        google.maps.drawing.OverlayType.CIRCLE,
-        google.maps.drawing.OverlayType.POLYGON,
-        google.maps.drawing.OverlayType.POLYLINE,
-        google.maps.drawing.OverlayType.RECTANGLE,
-      ],
+      drawingModes: [google.maps.drawing.OverlayType.POLYGON],
     },
     markerOptions: {
       icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
@@ -44,6 +97,12 @@ function initMap(): void {
   });
 
   drawingManager.setMap(map);
+  google.maps.event.addListener(drawingManager, 'polygoncomplete', function(polygon) {
+    const coords = polygon.getPath().getArray().map(({lat, lng}) => ({lat: lat(), lng: lng()}));
+  
+    console.log(JSON.stringify(coords, null, 1));
+    // #2 - Salvar coordenadas aqui
+  });
 }
 
 declare global {
